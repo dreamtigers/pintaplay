3a60755329e3da8fa54302ea8d7f725470fd2310
Ivan Gonzalez <ivcore@users.noreply.github.com>
2017-11-28 20:53:55 -0400

    Add the voting function.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,3 @@
 class Pin < ApplicationRecord
+  acts_as_votable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pins#index"
   resources :pins
+  put 'like', to: 'pins#upvote'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
